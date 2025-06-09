4cf74858700e483b9f602d2e31bc0d63b96717d3
Johan Morales Ortegon <johan.morales@celuweb.com>
2025-06-09 15:33:12 -0500

    feat: enhance recipe search functionality; implement dynamic search term binding and update displayed recipes logic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,6 +20,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   readonly initialMobileCount = 4;
   totalRecipes = 0;
 
+  searchTerm: string = '';
+
   private recipesSubscription: Subscription | undefined;
 
   constructor(private recipeService: RecipeService) {}
@@ -29,7 +31,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       (recipes: Recipe[]) => {
         this.allPopularRecipes = recipes;
         this.totalRecipes = this.allPopularRecipes.length;
-        this.updateVisibleRecipes();
+        this.updateDisplayedRecipes();
       },
       error => {
         console.error('Failed to load recipes in home component:', error);
@@ -49,21 +51,30 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
-    this.updateVisibleRecipes();
+    if (!this.searchTerm) {
+      this.updateDisplayedRecipes();
+    }
   }
   
   toggleRecipes(): void {
     this.isExpanded = !this.isExpanded;
-    this.updateVisibleRecipes();
+    this.updateDisplayedRecipes();
   }
 
-  private updateVisibleRecipes(): void {
-    if (this.isExpanded) {
-      this.visibleRecipes = this.allPopularRecipes;
+  updateDisplayedRecipes(): void {
+    if (this.searchTerm.trim() !== '') {
+      const lowercasedTerm = this.searchTerm.toLowerCase().trim();
+      this.visibleRecipes = this.allPopularRecipes.filter(recipe =>
+        recipe.name.toLowerCase().includes(lowercasedTerm)
+      );
     } else {
-      const isDesktop = window.innerWidth >= 768;
-      const count = isDesktop ? this.initialDesktopCount : this.initialMobileCount;
-      this.visibleRecipes = this.allPopularRecipes.slice(0, count);
+      if (this.isExpanded) {
+        this.visibleRecipes = this.allPopularRecipes;
+      } else {
+        const isDesktop = window.innerWidth >= 768;
+        const count = isDesktop ? this.initialDesktopCount : this.initialMobileCount;
+        this.visibleRecipes = this.allPopularRecipes.slice(0, count);
+      }
     }
   }
 }
